430161d7e4763153cec444046f55906d1c611b49
harnoorhuda08-dotcom <harnoor.huda08@gmail.com>
2026-02-25 11:33:05 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div style={{padding:40, fontFamily:"Arial"}}>
+      <h1>Tally Intelligence</h1>
+      <p>Virtual CFO Prototype</p>
+      <p>This is my GenAI Product Prototype for Tally.</p>
+    </div>
+  )
+}
